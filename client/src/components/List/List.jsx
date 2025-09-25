3c7e40bcfd451153804c15a03605fdd2b71fbfde
Your Name <you@example.com>
2025-09-25 22:55:14 +0300

    List

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
 import "./list.css";
+
+import { useState, useEffect, useCallback } from "react";
+
 import { filterPost } from "../../services/filterFunc";
 import SearchComponent from "../SearchComponent/SearchComponent";
 import NothingNot from '../PostList/NothingNot/NothingNot'
@@ -7,108 +9,101 @@ import PostsListOkView from "../PostList/PostsListOkView/PostsListOkView";
 import { getPostsByContextByRoleByStatus } from "../../services/ApiToServer/posts";
 import { useMyContext } from "../../services/MyProvider/MyProvider";
 
-/**
- * React component, which creates a platform for working with student posts.
- * @returns post board
- */
+
 const List = ({ ready, type, data }) => {
-	// State for posts list
-	const [postsList, setPostsLists] = useState([]);
 
-	const { contextState, updateContextState } = useMyContext();
-
-	// State for filtered posts list.
-	const [filteredPostsList, setFilteredPostsLists] = useState([]);
-
-	// State for search item.
+	const [postsList, setPostsList] = useState([]);
+	const [filteredPostsList, setFilteredPostsList] = useState([]);
 	const [searchItem, setSearchItem] = useState("");
-	const prepareData = async () => {
+
+	const { contextState } = useMyContext();
+
+	const prepareData = useCallback(async () => {
+		if (!ready) return;
 		if (contextState.role === 'admin') {
 			if (data.role === 'student') {
 				const posts = await getPostsByContextByRoleByStatus(data.role, data.role_context, data.status);
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (data.role === 'teacher') {
 				const posts = await getPostsByContextByRoleByStatus(data.role, null, data.status);
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (data.role === 'city') {
 				const posts = await getPostsByContextByRoleByStatus(data.role, data.role_context, data.status);
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (data.role === 'form') {
 				const posts = await getPostsByContextByRoleByStatus(data.role, data.role_context, data.status);
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (data.role === 'all') {
 				const posts = await getPostsByContextByRoleByStatus(data.role, null, data.status);
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 		}
 		else {
 			if (type === 'city') {
 				const posts = await getPostsByContextByRoleByStatus('city', contextState.city.split(), "1")
-				setPostsLists(posts)
-				setFilteredPostsLists(posts)
+				setPostsList(posts)
+				setFilteredPostsList(posts)
 				return ''
 			}
 			if (type === 'student') {
 				const posts = await getPostsByContextByRoleByStatus('student', localStorage.getItem('group').split(' '), "1")
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (type === 'teacher') {
 				const posts = await getPostsByContextByRoleByStatus("teacher", "null", "1");
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (type === 'all') {
 				const posts = await getPostsByContextByRoleByStatus("all", "null", "1");
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 			if (type === 'form') {
 				const posts = await getPostsByContextByRoleByStatus("form", localStorage.getItem('form'), "1");
-				setPostsLists(posts);
-				setFilteredPostsLists(posts);
+				setPostsList(posts);
+				setFilteredPostsList(posts);
 				return ''
 			}
 		}
-	}
+	})
 
 	useEffect(() => {
 		prepareData()
 	}, [contextState.city])
 
-	// After the page loads, return prepareData()
 	useEffect(() => {
 		prepareData()
 	}, [data]);
 
-	// Function to record the value, define and modify the filtered list.
 	function handleSearch(value) {
 		setSearchItem(value);
-		setFilteredPostsLists(filterPost(value, postsList));
+		setFilteredPostsList(filterPost(value, postsList));
 	}
 
 	return (
 		<div className="account">
-			<SearchComponent searchItem={searchItem} handleSearch={handleSearch} />
+			<SearchComponent searchItem={searchItem} handleSearch={handleSearch}  />
 			<div className="posts center">
-				{filteredPostsList != 0 ? <PostsListOkView filteredPostsList={filteredPostsList} setFilteredPostsLists={setFilteredPostsLists} /> : <NothingNot />}
+				{filteredPostsList != 0 ? <PostsListOkView filteredPostsList={filteredPostsList} setFilteredPostsLists={setFilteredPostsList} /> : <NothingNot />}
 			</div>
 		</div>
 	);
